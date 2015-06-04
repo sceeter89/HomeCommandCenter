2121bee702a36924f3f29cb3e5b0c9ac6762a777
Adam Marszalek <adam.marszalek@gmail.com>
2015-06-04 18:11:51 +0200

    Updated README and implemented more functionality to core application

--- README.md
+++ README.md
@@ -21,20 +21,20 @@ Below you will find list of all _built-in_ sensors:
 * `errors` - value is list of all exceptions that occured during current run. It's cleared every iteration.
 * `now` - contains datetime of current loop start. It's identical for every motor.
 * `disabled_plugins` - list containing keys of all plugins that were disabled because of exceeded number of failures.
-* `termination` - if present, means that system is about to shut down, and contains tuple in following format: ```python
- (terminator's key, terminator's type name, reason string)
- ```
+* `termination` - if value is not `None`, means that system is about to shut down, and contains tuple in following format: ```python
+  (terminator's key, terminator's type name, reason string)
+  ```
 * `runtime` - is a dictionary containing technical information about application. Following keys are available:
- * `start_time` - datetime object when application was launched
- * `loop_counter` - counter incremented with every loop of service
- * `last_loop_time` - duration of last iteration in milliseconds
- * `average_loop_time` - average loop duration in milliseconds
- * `errors` - dictionary where keys are plugin keys, and values are lists of thrown exceptions by those plugins
+  * `start_time` - datetime object when application was launched
+  * `loop_counter` - counter incremented with every loop of service
+  * `last_loop_time` - duration of last iteration in seconds
+  * `average_loop_time` - average loop duration in seconds
+  * `errors` - dictionary where keys are plugin keys, and values are lists of thrown exceptions by those plugins
 
 *Motor* - motor is a plugin, that basing on current sensors' state and own internal state, should perform some actions.
- For example when temperature remains too high, air conditioner might be turned on to drop it. Motors are loaded 
- similarly to sensors - using [Yapsy](http://yapsy.sourceforge.net), and reside in `plugins/motors` directory. There are
- no built-in motors.
+For example when temperature remains too high, air conditioner might be turned on to drop it. Motors are loaded 
+similarly to sensors - using [Yapsy](http://yapsy.sourceforge.net), and reside in `plugins/motors` directory. There are
+no built-in motors.
 
 ### Application flow
 Sensors, then motors, if too much fails then disabled and logged information about all exceptions. Conditions when
@@ -45,23 +45,28 @@ To develop custom sensor or motor you need to create a yapsy-plugin, which basic
 1. Copy template from `plugin/templates` directory, and give it appropriate name, .yapsy-plugin file must have name
  matching directory name.
 1. Update information in yapsy-plugin file
- 1. Mandatory fields are in `Core` section:
-  1. _Name_ - plugin's name, that will be logged if something noticeable happens to plugin
-  1. _Module_ - name of plugin's directory, or file name if it's single file plugin
-  1. _Key_ - short-hand, unique name, that will be used to clearly identify plugin. Only one plugin with given name will
-   be loaded. This is also string that motors will use to access some concrete sensor from system state. It also means,
-   that you cannot enter key that matches _built-in_ sensor name. Keys are case-insensitive.
+  1. Mandatory fields are in `Core` section:
+    1. _Name_ - plugin's name, that will be logged if something noticeable happens to plugin
+    1. _Module_ - name of plugin's directory, or file name if it's single file plugin
+    1. _Key_ - short-hand, unique name, that will be used to clearly identify plugin. Only one plugin with given name will
+       be loaded. This is also string that motors will use to access some concrete sensor from system state. It also means,
+       that you cannot enter key that matches _built-in_ sensor name. Keys are case-insensitive.
+    1. _Last chance_ - this parameter is valid only for motor plugins. If you set this parameter to `True` then this motor
+       will be executed during last loop when application is terminated. This might be useful for motors to shutdown
+       corresponding devices or send notification that system is shutting down.
 
 Generally `command_center` will "swallow" all exceptions thrown in sensor/motor, log them and eventually disable plugin 
 if it does not work properly. There are two exceptions:
 1. If you press Ctrl+C, then `command_center` will shutdown gracefully, so you may kill it from console.
 1. If, for some reason, you really want application to stop (probably due to some hardware failure, or alert) you can
-throw `TerminateApplication` error which will force application to stop.
+   throw `TerminateApplication` error which will force application to stop.
+
 Shutdown procedure is as follows:
 1. Finish current loop
-1. Set `termination` sensor
-1. Execute final loop
-1. Terminate
+1. Set `termination` _built-in_ sensor
+1. Execute final loop only for motors declared as _last chance_. When you declare such a motor please notice, that in
+   last loop state will only contain _built-in_ sensors, so be prepared for missing custom sensors state. 
+1. Terminate application
 
 ### Sensors
 Available sensors out of the box.
--- command_center.py
+++ command_center.py
@@ -1,38 +1,103 @@
 from collections import namedtuple
 from api.exceptions import TerminateApplication
-
-import time
-from datetime import datetime
-import logging
-from yapsy.PluginManager import PluginManager
-
 from api.sensor import Sensor
 from api.motor import Motor
 
-# logging.basicConfig(level=logging.DEBUG)
-PluginDetails = namedtuple('PluginInfo', ['name', 'key', 'instance', 'path'])
+import time
+import logging
+from datetime import datetime, timedelta
+from yapsy.PluginManager import PluginManager
+
+PluginDetails = namedtuple('PluginInfo', ['name', 'key', 'instance', 'wants_last_chance', 'path'])
+ALLOWED_UNHANDLED_EXCEPTIONS_PER_PLUGIN = 10
 
 
 def start_main_loop(sensors, motors):
-    disabled_plugins = []
-    while True:
-        try:
-            state = {
-                'errors': [],
-                'now': datetime.now()
-            }
-            for name, plugin in sensors:
-                try:
-                    state[name] = plugin.get_state()
-                except Exception as e:
-                    state['errors'].append(e)
+    disabled_plugins = set()
+    total_loops_duration = timedelta()
+    runtime_stats = {
+        'start_time': datetime.now(),
+        'loop_counter': 0,
+        'errors': {},
+        'average_loop_duration': timedelta(seconds=0),
+        'last_loop_duration': timedelta(seconds=0)
+    }
+    termination = None
+    while termination is None:
+        loop_start = datetime.now()
+        state = {
+            'errors': [],
+            'now': datetime.now(),
+            'runtime': runtime_stats,
+            'disabled_plugins': disabled_plugins,
+            'termination': termination
+        }
+        for plugin in sensors:
+            if plugin.key in disabled_plugins:
+                continue
 
-            for name, plugin in motors:
-                plugin.on_trigger(state)
-        except TerminateApplication:
+            try:
+                state[plugin.name] = plugin.instance.get_state()
+            except TerminateApplication as exception:
+                termination = (plugin.key, type(plugin.instance), exception.reason)
+            except KeyboardInterrupt:
+                termination = (plugin.key, type(plugin.instance), "User interruption")
+            except Exception as exception:
+                runtime_stats['errors'].get(plugin.key, []).append(exception)
+                state['errors'].append(exception)
+
+        for plugin in motors:
+            if plugin.key in disabled_plugins:
+                continue
+            try:
+                plugin.instance.on_trigger(state)
+            except TerminateApplication as exception:
+                termination = (plugin.key, type(plugin.instance), exception.reason)
+            except KeyboardInterrupt:
+                termination = (plugin.key, type(plugin.instance), "User interruption")
+            except Exception as exception:
+                runtime_stats['errors'].get(plugin.key, []).append(exception)
+                state['errors'].append(exception)
+
+        loop_stop = datetime.now()
+        loop_duration = loop_stop - loop_start
+        total_loops_duration += loop_duration
+        runtime_stats['loop_counter'] += 1
+        runtime_stats['average_loop_duration'] = total_loops_duration.total_seconds() / runtime_stats[
+            'loop_counter']
+        runtime_stats['last_loop_duration'] = loop_duration
+
+        for failing_plugin in runtime_stats['errors']:
+            if failing_plugin in disabled_plugins:
+                continue
+
+            if len(runtime_stats['errors'][failing_plugin]) > ALLOWED_UNHANDLED_EXCEPTIONS_PER_PLUGIN:
+                disabled_plugins.add(failing_plugin)
+
+        if len(disabled_plugins) == len(sensors) + len(motors):
+            logging.warning('All plugins have been disabled. Terminating application..')
             break
 
         time.sleep(0.2)
+
+    logging.info("Initiating shutdown procedure...")
+    terminal_state = {
+        'now': datetime.now(),
+        'runtime': runtime_stats,
+        'disabled_plugins': disabled_plugins,
+        'termination': termination
+    }
+    for plugin in motors:
+        if plugin.key in disabled_plugins or not plugin.wants_last_chance:
+            continue
+
+        try:
+            plugin.instance.on_trigger(terminal_state)
+        except Exception as exception:
+            runtime_stats['errors'].get(plugin.key, []).append(exception)
+
+    logging.info("Shutdown complete.")
+    logging.info(repr(runtime_stats))
 
 
 def collect_all_plugins():
@@ -44,9 +109,10 @@ def collect_all_plugins():
     for plugin in plugin_manager.getAllPlugins():
         name = plugin.name
         key = plugin.details.get('core', {}).get('key', None)
+        wants_last_chance = plugin.details.get('core', {}).get('last-chance', False).lower() == "true"
         instance = plugin.plugin_object
         path = plugin.path
-        yield PluginDetails(name, key, instance, path)
+        yield PluginDetails(name, key, instance, wants_last_chance, path)
 
 
 def load_plugins(all_plugins):
@@ -81,4 +147,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.DEBUG,
+                        format='[%(asctime)s][%(relativeCreated)d][%(levelname)s][%(module)s] %(message)s')
+    try:
+        main()
+    except Exception as e:
+        logging.error('Unexpected error occurred. If you believe issue is related to some bug in application, ' +
+                      'please open issue with exception details at https://github.com/sceeter89/command-center/issues',
+                      exc_info=e)
